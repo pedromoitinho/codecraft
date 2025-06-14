26907f3a6556c75ce4574645ebd9069b778bd518
Pedro Moitinho <pedrohenriquemoitinho.prado@ucsal.edu.br>
2025-06-14 17:56:57 -0300

    fix build error

--- src/contexts/LoadingContext.tsx
+++ src/contexts/LoadingContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, ReactNode, useContext, useState } from 'react';
+import { createContext, useContext, useState, type ReactNode } from 'react';
 
 interface LoadingContextType {
   isLoading: boolean;
